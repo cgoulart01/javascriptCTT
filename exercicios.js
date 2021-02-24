1f28da45ce42182d9ff93428bb82bcf312505988
cgoulart01 <cgoulart.rj@gmail.com>
2021-02-24 23:32:43 +0000

    commit inicial

---
+++ exercicios.js
@@ -0,0 +1,26 @@
+function helloWorld() {
+	console.log('Hello World')
+}
+helloWorld()
+function helloName(nome) {
+	console.log('Hello ' + nome)
+}
+helloName('Caio')
+
+function somaNumero(a, b) {
+	resultado = a + b;
+	console.log(resultado)
+}
+somaNumero(2,3)
+function somaString(a, b) {
+const number1=Number(a);
+const number2=Number(b);
+	if (typeof number1==='number'&&typeof number2==='number'){
+		soma =Number(a) + Number(b)
+	}
+	 if(number1==='NaN'||number2!=='NaN'){
+		soma = null
+	}
+	console.log(soma)
+}
+somaString(2,'a')
